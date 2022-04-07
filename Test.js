8b0429cf9ccadcfef9c64d0bd0a5b10a0e8b673f
Olexburg <wonderwallius@gmail.com>
2022-04-07 20:34:08 +0300

    Topic 6 Types

--- Test.js
+++ Test.js
@@ -1,6 +1,19 @@
 let variable=1;
 const test=1;
-
 console.log(variable);
 console.log(test);
+
+let number = 1;
+let string = "1";
+let boolean = 2;
+let value = null;
+let Alex = undefined;
+console.log(number + string);
+console.log(boolean - value);
+console.log(boolean * Alex);
+console.log(null / number);
+
+let str = "777";
+let num = Number(str);
+console.log(str);
 
