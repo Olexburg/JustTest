9c288b8df4090f524a28a14e54b065fe239164b0
Olexburg <wonderwallius@gmail.com>
2022-04-09 21:21:00 +0300

    Topic 6 Types

---
+++ Topic 7 (operators).js
@@ -0,0 +1,85 @@
+let a = 134;
+a = -a;
+console.log(a);
+
+let f = 30, b = 31;
+console.log(f + b);
+
+let q = 30, e = 31;
+console.log(q - e);
+
+let w = 30.45, r = 30.45;
+console.log(w - r);
+
+let z = 3, x = true;
+console.log(z / x);
+
+let aa = 100, bb = 3;
+console.log(aa % bb);
+
+let Alex = 4;
+console.log (Alex ** 1/4);
+
+let y = "WBC" + "Project";
+console.log(y);
+
+let Kolya = 37, Vasya = 2, Anya = "37";
+console.log(Kolya + Vasya + Anya);
+
+console.log(+null);
+
+let c = "3", v = "4";
+console.log(+c + +v);
+
+let i = (3 ** 2 / -3 + +4)
+console.log(i);
+
+let s = 5;
+f = s;
+g = s;
+s = 1;
+console.log(s + f + g);
+
+let k = 4;
+k *= 7;
+console.log(k + 7);
+
+let o = 4;
+o *= 7 - 5;
+console.log(o);
+
+let n = 1, m = 2;
+n++;
+m--;
+console.log (m + n);
+
+let h = 1, j = 2;
+let hh = ++h;
+let jj = j++;
+console.log (hh + jj);
+
+let aaa = (2 + 1 > 2);
+console.log(aaa);
+
+let bbb = (2 === 2);
+console.log(bbb);
+
+console.log(a < z);
+
+console.log('Abcd' > 'A');
+
+console.log('374' > 375);
+
+console.log('376' >= 375);
+
+console.log(0 !== true);
+
+
+
+
+
+
+
+
+
+
