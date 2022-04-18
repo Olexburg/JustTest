6592c792e38c1b5c0ae5c8d6476f1e7bb38886f1
Olexburg <wonderwallius@gmail.com>
2022-04-18 11:29:54 +0300

    исправления к циклу и функциям

--- Topic 11 Cycles.js
+++ Topic 11 Cycles.js
@@ -1,6 +1,33 @@
 // Цикл, который будет выводить число а, пока оно не станет меньше 10
+
+//1 вариант
 let a = 0
-while (a < 10) {
-    console.log(a)
+while (a < 9) {
     a++
+    console.log(a)
 }
+
+// через for
+for (let b = 1; b < 10; b++) {
+    console.log(b)
+}
+
+// через рекурсию
+let i = 1
+function recurs() {
+    console.log(i)
+    i++
+    if (i < 10) {
+        recurs()
+    }
+}
+    recurs()
+
+
+
+
+
+
+
+
+
--- Topic 12 (functions).js
+++ Topic 12 (functions).js
@@ -1,25 +1,42 @@
 // 1. Функция сложения двух чисел
 const forSum = (a, b) => a + b
 console.log(forSum(3, 32))
-// написал через стрелки
 
+function forSum2 (a, b) {
+    return (a + b)
+    }
+console.log(forSum2(3, 32))
 
 // 2. Функция определения имени (если имя ваше, то привет + имя)
 function showAlex(name) {
-    let message = 'Привет ' + name
-    if (name === 'Alex')
-    console.log(message);
+    const message = 'Привет ' + name
+    if (name === 'Alex') {
+        console.log(message)
+    }
 }
 showAlex('Alex')
 
 // 3. Функция вычисления типа аргумента и вывод в консоль
-const typeArg = (text) => console.log(typeof text)
+const typeArg = text => console.log(typeof text)
 typeArg(true)
 
-// 4/ Функция выбора четных элементов массива (возвращает новый массив)
-let cars = ['Ауди', 'Порш', 'Мазда', 'Киа']
+function typeArg2 (text) {
+    console.log(typeof text)
+}
+typeArg2('test')
+
+// 4. Функция выбора четных элементов массива (возвращает новый массив)
+const cars = ['Ауди', 'Порш', 'Мазда', 'Киа']
  function forEven(item, index) {
     return index%2 !==0
 }
-let filtered = cars.filter(forEven)
+const filtered = cars.filter(forEven)
 console.log(filtered)
+
+const arr = ['Ауди', 'Порш', 'Мазда', 'Киа']
+function getEven(arr) {
+    return arr.filter((item, index) => index%2 !==0)
+}
+console.log(getEven(arr))
+
+
